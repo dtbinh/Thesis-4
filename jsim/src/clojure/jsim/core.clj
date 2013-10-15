32bb01bdcfc83f06815d25422034899a4f9051b1
John Downs <john.downs@maine.edu>
2013-10-15 00:04:39 -0400

    added user stories

--- jsim/src/clojure/jsim/core.clj
+++ jsim/src/clojure/jsim/core.clj
@@ -1,4 +1,29 @@
-(ns jsim.core)
+(ns jsim.core
+  (:require [[jsim.robot]
+
+                        ]))
+
+(defn run-step [])
+
+(defn run-cli []
+  (let [sensor (make-sensor params)
+        bot (make-robot params)
+        waypoints (make-waypoints params)
+        landmarks (make-landmarks params)
+        bayes-filter (make-ekf params)]
+   (run-robot) ; get groundtruth
+   (add-noise groundtruth)
+   (add-noise observations)
+   (run-simulator))
+           
+
+(defn -main [& args]
+; Run CLI or Web?
+; Read config
+; Run simulator
+; Save results
+)
+
 
 (defrecord Coordinate "Cartesian Coordinate" [x y])
 
