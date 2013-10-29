db6cb570820e22fc4840a3a327e85c504928c2bf
John Downs <john.downs@maine.edu>
2013-10-29 00:22:40 -0400

    test improvements

--- jsim/src/clojure/jsim/sensor.clj
+++ jsim/src/clojure/jsim/sensor.clj
@@ -1,50 +1,67 @@
 (ns jsim.sensor
-  (:require [clojure.core.matrix :refer :all]))
+  (:require [clojure.core.matrix :as mat]))
 
 (defprotocol Sensor
-  (get-visible-landmarks [feature-map pose])
-  (observe-landmarks [feature-map pose landmarks])
-  (observe [pose landmark])
-  (observe' [pose landmark])
-  (is-visible? [feature-map pose landmark]))
+  (get-visible-landmarks [sensor feature-map pose])
+  (observe-landmarks [sensor feature-map pose landmarks])
+  (observe [sensor pose landmark])
+  (observe' [sensor pose landmark])
+  (is-visible? [sensor landmark]))
 
 (defn wrap [a]
-  (Math/atan2 (sin a) (cos a)))
+  (Math/atan2 (Math/sin a) (Math/cos a)))
 
 (defn rotate [p a]
   (let [cosa (Math/cos a)
         sina (Math/sin a)
         -sina (- sina)
         R [[cosa -sina]
-           [sina  cosa]]]))
+           [sina  cosa]]]
+   (mat/scale R p)))
+
+(defn translate [p f]
+  (mat/add p f))
 
 (defn transform [point frame]
-  )
+  (let [f [(first frame) (second frame)]
+        a (nth frame 2)]
+    (translate (rotate point a) f)))
 
-(deftype RangeBearingSensor [range arc noise]
+(defrecord RangeBearingSensor [range arc noise]
   Sensor
-  (get-visible-landmarks [feature-map pose])
+  (is-visible?
+    [this landmark]
+    (let [x (first landmark)
+          y (second landmark)
+          d (Math/sqrt (+ (* x x) (* y y)))
+          bearing #(Math/atan2 y x)
+          range (wrap (+ (:range this) (/ Math/PI 2)))
+          arc (:arc this)]
+      (and (<= d range)
+           (<= bearing arc))))
 
-  Sensor
-  (observe-landmarks [feature-map pose landmarks]
-    (map #(observe pose %)
-         (get-visible-landmarks feature-map pose)))
+  (get-visible-landmarks [this feature-map pose]
+    (let [ct (/ (count feature-map) 3)
+          feature-map' (mat/reshape feature-map [ct 3])
+          landmarks (map #(transform % pose) feature-map')
+          visible? (partial is-visible? this)]
+      (filter visible? landmarks)))
 
   (observe
-    [pose landmark]
+    [this pose landmark]
     (let [dx (- (first landmark) (first pose))
           dy (- (second landmark) (second pose))
           a (nth pose 3)
           d [dx dy]
-          q (dot d d)
+          q (mat/dot d d)
           id (nth landmark 3)]
       [(Math/sqrt q)
        (wrap (- (Math/atan2 dy dx) a))
        id]))
+  (observe-landmarks [this feature-map pose landmarks]
+    (map #(observe this pose %)
+         (get-visible-landmarks this feature-map pose)))
 
   (observe'
-    [pose landmark]
-    nil)
-
-  (is-visible?
-    [feature-map pose landmark]))
+    [this pose landmark]
+    nil))
--- jsim/test/jsim/sensor_test.clj
+++ jsim/test/jsim/sensor_test.clj
@@ -1,6 +1,13 @@
 (ns jsim.sensor-test
   (:require [clojure.test :refer :all]
-            [jsim.sensor :refer :all]))
+            [jsim.sensor :refer :all]
+            [clojure.core.matrix.operators :as ops]))
+
+(defn nearly=
+  "a and b are nearly equal elementwise"
+  [a b]
+  (let [c (ops/- a b)]
+    (map #(> 1E-16 (Math/abs %)) c)))
 
 (deftest test-wrap
   (testing "Wrap function keeps angles normalized"
@@ -11,6 +18,12 @@
                  (<= (wrap a) (* Math/PI 2))
                  (>= (wrap a) (0))))
             (recur (+ a 0.1))))))
+
+(deftest test-rotate
+  (testing "Matrix rotation"
+    (let [point [1 0]
+          a (/ Math/PI 2)]
+      (is (nearly= [0 1] (rotate point a))))))
 
 (deftest test-get-visible-landmarks
   (testing "All visible landmarks are returned as a matrix"
@@ -23,8 +36,8 @@
                        0 -1 6
                        2 0 7]
           pi_2 (/ Math/PI 2)
-          sensor (RangeBearingSensor. pi_2)]
-      (is (= [[1 0 1]
-              [1 1 2]
-              [-1 -1 5]]
-             (get-visible-landmarks feature-map pose))))))
+          sensor (jsim.sensor/->RangeBearingSensor 1 pi_2 0)
+          expected [[1 0 1] [1 1 2] [-1 -1 5]]
+          actual (get-visible-landmarks sensor feature-map pose)]
+      (println actual)
+      (is (ops/== expected actual)))))
